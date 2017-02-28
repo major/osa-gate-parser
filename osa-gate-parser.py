dd4c621ca9f579f4723f4725cf209dd4fe84a88a
Major Hayden <major@mhtx.net>
2017-02-28 11:14:51 -0600

    Use wallclock time instead

--- osa-gate-parser.py
+++ osa-gate-parser.py
@@ -12,8 +12,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# -----------------------------------------------------------------------------
+#
+# Usage ./osa-gate-parser.py <GATE_CONSOLE_URL>
+#
 """Parse OSA gate jobs and report on timings."""
-import datetime
+import operator
 import re
 import sys
 from dateutil.parser import parse
@@ -34,84 +39,80 @@ class GateParser:
         for line in self.r:
             self.process_line(line)
 
+        self.calculate_duration()
+
     def process_line(self, raw_line):
         """Process a line from the console output."""
-        line = raw_line[29:]
+        try:
+            timestamp, line = raw_line.split(" | ", 1)
+        except ValueError:
+            return False
 
-        # Are we at the beginning of a play?
-        match = re.search(r'PLAY \[(.*)\] \**', line)
-        if match is not None:
-            self.handle_play(match.groups()[0])
+        # Handle each task line
+        if line.startswith("TASK") or line.startswith('RUNNING HANDLER'):
+            self.handle_task(timestamp, line)
             return True
 
-        # Are we at the beginning of a task?
-        if line.startswith('TASK '):
-            self.handle_task(line)
+        # When we get to the PLAY RECAP line, we need to write the final
+        # time to the last task we saw and flush the name of the previous task.
+        if line.startswith("PLAY RECAP"):
 
-    def handle_play(self, play_name):
-        """Handle the PLAY lines in the output."""
-        if self.current_play != play_name:
+            # Sometimes we get two play recaps in a row without tasks between
+            # them.
+            if self.previous_task is None:
+                return True
+
+            self.stats[self.previous_task].append(parse(timestamp))
             self.previous_task = None
+            return True
 
-        self.current_play = play_name
-        if play_name not in self.stats.keys():
-            self.stats[play_name] = {}
-
-    def handle_task(self, line):
+    def handle_task(self, timestamp, line):
         """Handle the TASK lines in the output."""
-        match = re.search(r"TASK \[(.*)\] \**", line)
-        task_name = match.groups()[0]
+        match = re.search(r"(TASK|RUNNING HANDLER) \[(.*)\] \**", line)
+        task_name = match.groups()[1]
 
-        # Get the next line which has the timing data
-        timing_line = self.r.next()[29:]
+        timestamp = parse(timestamp)
 
-        # Ensure we got timing data -- if not, get the next one
-        if timing_line.startswith('task path'):
-            timing_line = self.r.next()[29:]
-
-        match = re.search(
-            r"^(\w+ \d\d \w+ \d+\s+\d+:\d+:\d+ \+[0-9]+)",
-            timing_line)
-        task_start = parse(match.groups()[0])
-
-        if self.previous_task is None:
-            pass
+        # Add a stats entry for the start of this task
+        if task_name in self.stats.keys():
+            self.stats[task_name].append(timestamp)
         else:
-            self.stats[self.current_play][self.previous_task]['elapsed'] = (
-                task_start - self.stats[self.current_play][self.previous_task]['start']
-            )
+            self.stats[task_name] = [timestamp]
 
-        self.stats[self.current_play][task_name] = {
-            'start': task_start
-        }
+        # Save the ending time for the previous task
+        if self.previous_task is not None:
+            self.stats[self.previous_task].append(timestamp)
 
         self.previous_task = task_name
 
-    def elapsed_sort(self, task):
-        """Sort function for sorting tasks by elapsed time."""
-        if 'elapsed' in task[1].keys():
-            return task[1]['elapsed']
-        else:
-            return datetime.timedelta(0)
+    def calculate_duration(self):
+        """Calculate the duration of each task."""
+        for task_name, timestamps in self.stats.items():
+            total_time = 0
+
+            # If we only got one timestamp, we likely found our first or last
+            # task. This needs to be handled better in the future. ;)
+            if len(timestamps) % 2 != 0:
+                self.stats[task_name] = 0
+                continue
+
+            # Create timestamp pairs and calculate the total time.
+            timestamp_pairs = [timestamps[x:x + 2]
+                               for x in range(0, len(timestamps), 2)]
+            for pair in timestamp_pairs:
+                total_time += (pair[1] - pair[0]).total_seconds()
+            self.stats[task_name] = total_time
 
     def display_output(self):
         """Pretty prints the stats dict."""
-        for play, tasks in self.stats.items():
-
-            # Print a header for the play
-            header = "PLAY: {} ".format(play).ljust(80, '-')
-            print(header)
-
-            # Sort the tasks and print them.
-            sorted_tasks = sorted(tasks.items(), key=self.elapsed_sort,
-                                  reverse=True)
-            for task, timings in sorted_tasks[:20]:
-                task = "{} | {}".format(
-                    str(timings.get(
-                        'elapsed',
-                        datetime.timedelta(0))).rjust(9),
-                    task)
-                print(task)
+        sorted_stats = sorted(
+            self.stats.items(),
+            key=operator.itemgetter(1),
+            reverse=True)
+        for task_name, total_time in sorted_stats[:50]:
+            minutes, seconds = divmod(total_time, 60)
+            pretty_time = "%02d:%02d" % (minutes, seconds)
+            print("{} - {}".format(pretty_time, task_name))
 
 
 x = GateParser(sys.argv[1])
